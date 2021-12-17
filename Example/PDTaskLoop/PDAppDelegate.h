f510948db0f5bd1d5f09158f9516b91fc95a16ea
liang <leiliang4@xdf.cn>
2021-12-17 17:40:26 +0800

    Initial commit

---
+++ Example/PDTaskLoop/PDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PDAppDelegate.h
+//  PDTaskLoop
+//
+//  Created by liang on 12/17/2021.
+//  Copyright (c) 2021 liang. All rights reserved.
+//
+
+@import UIKit;
+
+@interface PDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/PDTaskLoop/PDViewController.m
@@ -0,0 +1,29 @@
+//
+//  PDViewController.m
+//  PDTaskLoop
+//
+//  Created by liang on 12/17/2021.
+//  Copyright (c) 2021 liang. All rights reserved.
+//
+
+#import "PDViewController.h"
+
+@interface PDViewController ()
+
+@end
+
+@implementation PDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
